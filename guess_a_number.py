4d0f0a5e15fa064bbf49f850067afd287613ca46
Bernard <gberna8254@greenville.k12.sc.us>
2017-09-18 14:13:44 -0400

    Started a new gamemode function that determines how it is played.

--- guess_a_number.py
+++ guess_a_number.py
@@ -4,31 +4,65 @@ import math
 #config
 low = 1
 high = 100
-guesses = 1
-x = 2 #this helps decide how many guesses the player needs
+tries = 1
 
-while x < high:
+#calculates the number of tries the player gets
+x = 2
+while x <= high:
     x = x*2
-    guesses = guesses + 1
+    tries = tries + 1
 
-#start game
-rand = random.randint(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+#helperfunction
+def get_guess():
+    while True:
+        guess = input("Take a guess: ")
 
-guess = -1
+        if guess.isnumeric():
+            guess = int(guess)
+            return guess
+        else:
+            print("You must enter a number.")
 
-while guess != rand and guesses > 0:
+def get_high():
+    #YOU LEFT OFF HERE
 
-    guess = input("Take a guess: ")
-    guess = int(guess)
+#choose a game mode
+print("Choose a gamemode. Select \"1\" or \"2\" for You Guess or Computer Guesses.")
+
+def get_gamemode():
+    while True:
+            guess = input("Take a guess: ")
+
+            if guess.isnumeric() and guess == "1" or guess == "2":
+                guess = int(guess)
+                return guess
+            else:
+                print("You must enter the number \"1\" or \"2\".")
+
+#start game 1
+if guess = 1:
+    rand = random.randint(low, high)
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+
+
+    guess = -1
+
+    while guess != rand and tries > 0:
+        guess = get_guess()
     
-    if guess < rand:
-        guesses = guesses - 1
-        print("You guessed too low. " + str(guesses) + " guesses left.")
-    elif guess > rand:
-        guesses = guesses - 1
-        print("You guessed too high. " + str(guesses) + " guesses left.")
-    else:
-        print("You got it!")
+        if guess < rand:
+            tries = tries - 1
+            print("You guessed too low. " + str(tries) + " tries left.")
+        elif guess > rand:
+            tries = tries - 1
+            print("You guessed too high. " + str(tries) + " tries left.")
+        else:
+            print("You got it!")
 
-print("Game over.")
+    print("Game over. The number was " + str(rand) + ".")
+
+#start game 2
+if guess = 2:
+    print("Before we start, I will need the range of the numbers.")
+    time.sleep(2)
+    get_high() = 
