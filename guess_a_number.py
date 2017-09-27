aaa68b52ecba0fd2937b9616e9ceb707c5efdf35
Bernard <gberna8254@greenville.k12.sc.us>
2017-09-27 14:04:22 -0400

    Finished the 'guess-a-number' game.

--- guess_a_number.py
+++ guess_a_number.py
@@ -13,8 +13,6 @@ print("        \/             \/      \/      \/                  \/            
 #config
 low = 0
 high = 0
-guess = -1
-playing_game = True
 
 #helper functions
 def get_guess():
@@ -31,88 +29,114 @@ def get_guess():
             print("You must enter a number.")
             print("")
 
-def restart():
-    while True:
-        print("Would you like to play again? (y/n)")
-        print("")
-        answer = input()
-        
-        if answer == "y":
-            return True
-        
-        elif answer == "n":
-            return False
-
-        else:
-            print("You must enter \"y\" or \"n\".")
-
-#start game
-while playing_game:
-
-    print("")
-    print("Before we begin, we'll need the highest and lowest numbers.")
-    print("")
-    
-
-    #calculates low
-    print("What is the lowest possible value I should guess?")
-    print("")
-
-    while True:
-        low = input()
-
-        if low.isnumeric():
-            low = int(low)
-            break
-        else:
-            print("")
-            print("You must enter a number.")
-            print("")
-
-    #calculates high
-    print("")
-    print("And the highest possible value I should guess?")
-    print("")
-
-    while True:
-        high = input()
-        if high.isnumeric():
-            if int(high) <= low:
-                print("")
-                print("Your number must be higher than the lowest value.")
-                print("")
-            else:
-                high = int(high)
-                break
-        else:
-            print("")
-            print("You must enter a number.")
-            print("")
-
-    #calculates number of tries the player gets
-    tries = math.log(high-low,2)
+#calculates number of tries the player gets
+def get_tries():
+    tries = math.log(high-low,2) + 1
     tries = round(tries)
+    return tries
 
-    guess = get_guess()
-
-    if guess == 10:
-        print("You win!")
-        print("")
-        
-        playing_game = restart()
-        
-    else:
-        print("You lose.")
-        print("")
-        
-        playing_game = restart()
+def rando_numero():
+    rando = random.randint(low,high)
+    rando = round(rando)
+    return rando
 
 #end game screen
-print("  ________                            ________                           ._.")
-print(" /  _____/ _____     _____    _ ___   \_____  \  ___  __  ____  _______  | |")
-print("/   \  ___ \__  \   /     \  _/ __ \   /   |   \ \  \/ /_/ __ \ \_  __ \ | |")
-print("\    \_\  \ / __ \_|  Y Y  \ \  ___/  /    |    \ \   / \  ___/  |  | \/  \|")
-print(" \______  /(____  /|__|_|  /  \___  > \_______  /  \_/   \___  > |__|     __")
-print("        \/      \/       \/       \/          \/             \/           \/")
+def end_game():
+    print("  ________                            ________                           ._.")
+    print(" /  _____/ _____     _____    _ ___   \_____  \  ___  __  ____  _______  | |")
+    print("/   \  ___ \__  \   /     \  _/ __ \   /   |   \ \  \/ /_/ __ \ \_  __ \ | |")
+    print("\    \_\  \ / __ \_|  Y Y  \ \  ___/  /    |    \ \   / \  ___/  |  | \/  \|")
+    print(" \______  /(____  /|__|_|  /  \___  > \_______  /  \_/   \___  > |__|     __")
+    print("        \/      \/       \/       \/          \/             \/           \/")
 
-time.sleep(5)
+    time.sleep(5)
+
+def restart():
+    print("")
+    print("Would you like you play again? (y/n)")
+    print("")
+    answer = input("")
+    if answer == 'y':
+        playing_game()
+        
+    elif answer == 'n':
+        end_game()
+        
+    else:
+        print("You must enter a \"y\" or \"n.\"")
+        print("")
+
+print("")
+print("Before we begin, we'll need the highest and lowest numbers.")
+print("")
+
+#calculates low
+print("What is the lowest possible value I should guess?")
+print("")
+
+while True:
+    low = input()
+
+    if low.isnumeric():
+        low = int(low)
+        break
+    else:
+        print("")
+        print("You must enter a number.")
+        print("")
+
+#calculates high
+print("")
+print("And the highest possible value I should guess?")
+print("")
+
+while True:
+    high = input()
+    if high.isnumeric():
+        if int(high) <= low:
+            print("")
+            print("Your number must be higher than the lowest value.")
+            print("")
+        else:
+            high = int(high)
+            break
+    else:
+        print("")
+        print("You must enter a number.")
+        print("")
+
+#start game
+def playing_game():
+
+    print("")
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) + "...")
+
+    tries = get_tries()
+
+    rando = rando_numero()
+
+    while tries > 0:
+
+        print("")
+        print("You have " + str(tries) + " tries left.")
+
+        guess = get_guess() 
+            
+        if guess == rando:
+            print("You win!")
+            tries = 0
+
+        elif guess < rando:
+            print("You guessed too low.")
+            tries = tries-1
+            
+        elif guess > rando:
+            print("You guessed too high.")
+            tries = tries-1
+
+        else:
+            print("You lose!")
+            
+    restart()
+    
+playing_game()
